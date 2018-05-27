a7169f2f9ce36261becccd3441f66bf0fcc60d89
Bernardo Duarte <bernardolageduarte@gmail.com>
2018-05-27 18:46:24 -0300

    Muda design pra usar MaterializeCSS e JQuery Falta fazer as telas secundárias de gerência e fazer os botões dentro das páginas funcionar

---
+++ app/scripts/navigation.js
@@ -0,0 +1,39 @@
+$(() => {
+    // default page to load
+    $("#page-content").load("home.html");
+
+    // Set mobile sidenav
+    $('.sidenav').sidenav();
+    
+    // load pages dynamically
+    $(".nav-home").click(() => {
+        $("#page-content").load("home.html");
+    });
+    $(".nav-store").click(() => {
+        $("#page-content").load("store.html");
+    });
+    $(".nav-services").click(() => {
+        $("#page-content").load("services.html");
+    });
+    $(".nav-management").click(() => {
+        $("#page-content").load("management.html");
+    });
+    $(".nav-manage-users").click(() => {
+        $("#page-content").load("management-users.html");
+    });
+    $(".nav-manage-produts").click(() => {
+        $("#page-content").load("management-products.html");
+    });
+    $(".nav-manage-services").click(() => {
+        $("#page-content").load("management-services.html");
+    });
+    $(".nav-account").click(() => {
+        $("#page-content").load("account.html");
+    });
+    $(".nav-cart").click(() => {
+        $("#page-content").load("shopping-cart.html");
+    });
+    $(".nav-login").click(() => {
+        $("#page-content").load("login.html");
+    });
+})
