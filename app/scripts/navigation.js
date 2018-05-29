3ee1e4ca58ccfa41c5567a2efa7cb8501f4c8bd4
Bernardo Duarte <bernardolageduarte@gmail.com>
2018-05-29 18:56:15 -0300

    Faz reload do script de navegação em todas página que carregar

--- app/scripts/navigation.js
+++ app/scripts/navigation.js
@@ -1,39 +1,35 @@
 $(() => {
-    // default page to load
-    $("#page-content").load("home.html");
-
-    // Set mobile sidenav
-    $('.sidenav').sidenav();
-    
     // load pages dynamically
     $(".nav-home").click(() => {
-        $("#page-content").load("home.html");
+        $("#page-content").load("home.html", () => reload());
     });
     $(".nav-store").click(() => {
-        $("#page-content").load("store.html");
+        $("#page-content").load("store.html", () => reload());
     });
     $(".nav-services").click(() => {
-        $("#page-content").load("services.html");
+        $("#page-content").load("services.html", () => reload());
     });
     $(".nav-management").click(() => {
-        $("#page-content").load("management.html");
+        $("#page-content").load("management.html", () => reload());
     });
     $(".nav-manage-users").click(() => {
-        $("#page-content").load("management-users.html");
+        $("#page-content").load("management-users.html", () => reload());
     });
     $(".nav-manage-produts").click(() => {
-        $("#page-content").load("management-products.html");
+        $("#page-content").load("management-products.html", () => reload());
     });
     $(".nav-manage-services").click(() => {
-        $("#page-content").load("management-services.html");
+        $("#page-content").load("management-services.html", () => reload());
     });
     $(".nav-account").click(() => {
-        $("#page-content").load("account.html");
+        $("#page-content").load("account.html", () => reload());
     });
     $(".nav-cart").click(() => {
-        $("#page-content").load("shopping-cart.html");
+        $("#page-content").load("shopping-cart.html", () => reload());
     });
     $(".nav-login").click(() => {
-        $("#page-content").load("login.html");
+        $("#page-content").load("login.html", () => reload());
     });
 })
+
+reload = () => $.getScript("scripts/navigation.js");
---
+++ app/scripts/script.js
@@ -0,0 +1,6 @@
+$(() => {
+    // default page to load
+    $("#page-content").load("home.html");
+    // Set mobile sidenav
+    $('.sidenav').sidenav();
+})
